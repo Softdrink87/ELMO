29ae427d0909d8f2bee522cdbd1869cb43915ea5
Softdrink87 <36359312+Softdrink87@users.noreply.github.com>
2021-07-06 16:42:12 +0900

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,31 @@
 from flask import Flask
+import ELMO
+import requests,time
 
 app = Flask(__name__)
 
+#mdl = ELMO(tokens="",tokenizer="",max_input_length="",UD_TAGS="")
+#mdl 에 있는 초기화 인자들 채워넣자  ! 
 
-@app.route('/test')
-def hello():
-    return "Test"
+
+@app.route('/POS',methods=['POST'])
+def POSpredict():
+    received_data = request.get_json()
+    received_tokens, received_tokenizer, received_max_input_length, received_UD_TAGS = 
+    received_data['token'],received_data['tokenizer'],received_data['max_input_length'],received_data['UD_TAGS']
+    mdl = ELMO(received_tokens,received_tokenizer,received_max_input_length,received_UD_TAGS)
+    mdl.POSprocess()
+    #start = time.time()
+    
+
+
+@app.route('/NSP',methods=['POST'])
+def NSPpredict():
+    received_data = request.get_json()
+    received_sentence, received_tokenized_sentence = received_data['sentence'],received_data['tokenized_sentence']
+    mdl.sentence = received_sentence
+    mdl.NSPprocess()
+
 
 
 if __name__ == '__main__':  # if it's an entry point. then run Flask :O -> if it's not a module call then run Flask :D
