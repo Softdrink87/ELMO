a18ca4c5c57e798de8aff91387cf1e4298265543
Softdrink87 <36359312+Softdrink87@users.noreply.github.com>
2021-07-09 15:25:45 +0900

    한글 버전 현재 에러 있음.. 추후에 고칠 예정

--- script/dataCreater.py
+++ script/dataCreater.py
@@ -9,7 +9,7 @@ def parsing(count,number):
   for i in range(1,count+1):
     targetUrl = urlopen("https://ko.wikipedia.org/wiki/%ED%8A%B9%EC%88%98:%EC%9E%84%EC%9D%98%EB%AC%B8%EC%84%9C") if number == 1 else urlopen("https://en.wikipedia.org/wiki/Special:Random")
     bsObje = BeautifulSoup(targetUrl, 'html.parser')
-    target = bsObje.select('a')
+    target = bsObje.select('p')
     print(str(i) + "... loading")
     for i in range(len(target)-1):
       result.append(target[i].get_text())
@@ -29,7 +29,6 @@ def parsing(count,number):
     
 
 
-
   for index in wiki_index:
       for i in range(len(result)):  
         if index in result[i]:
@@ -43,12 +42,48 @@ def saveParser(repeat,number):
   result = json.dumps(parsing(repeat,number),indent=4)
   with open ('data.txt','w',newline='\n') as f:
     f.write(result)
-  print('완료')
+  #print('완료')
+
+def remove_unicode():
+  data = []
+  result = []
+  with open('data.txt','r') as d:
+    for i in d:
+      if '\\' in i:
+        i = i.replace('\\','/') 
+      data.append(i)
+  os.remove('data.txt')
+
+  for i in data:
+    res = re.sub(r"/[u]\S{4,10}","",i)
+    result.append(res)
+
+  #dumps = json.dumps(result)
+  #print(dumps)
+
+  with open('result.txt','w') as f:
+    for i in result:
+      if r'\n' in i:
+        i = i.replace(r'\n','')
+      if '/' in i:
+        i = i.replace('/','')
+      f.write(i)
+    print('완료 : result.txt 파일을 확인해보세요.')
+
+
+    
+  
+  
+
 
 print('------------------ auto parser with BeautifulSoup4 ---------------------------',end='\n')
-print('1. 한국 위키피디아',end='\n')
+print('1. 한국 위키피디아 !!!현재 불안정한 상태! 사용 비권장 추후 고칠 예정!!!',end='\n')
 print('2. 미국 위키피디아',end='\n')
 print('선택 :',end='\n')
 routeinput = int(input())
-repeat = int(input('반복 횟수 선택 :'))
+repeat = int(input('크롤링 할 문서 개수 선택 :'))
+if (os.path.isfile('./data.txt') and os.path.isfile('./result.txt')):
+  os.remove('./data.txt')
+  os.remove('./result.txt')
 saveParser(repeat,routeinput)
+remove_unicode()
